49898f65058c521f2ea8d3b15b2426b52db49b56
KermHack <98063322+Kermhack5@users.noreply.github.com>
2024-01-05 20:39:35 +0100

    Update Telegraph.js

--- commands/Telegraph.js
+++ commands/Telegraph.js
@@ -4,9 +4,9 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
+ * @project_name : Kerm MD 2.0
+ * @author : Kermhack5 <https://github.com/Kermhack5/Kerm-MD-2.0>
+ * @description : Kerm MD 2.0,A Multi-functional whatsapp bot.
  * @version 0.0.6
  **/
 //---------------------------------------------------------------------------
